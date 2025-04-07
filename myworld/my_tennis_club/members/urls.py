85476e1da2d855196b715415a29b8f76991c8731
DavidWu-Shaw <david_wu_lf@hotmail.com>
2025-04-07 17:49:25 -0400

    finish member site with master page

--- myworld/my_tennis_club/members/urls.py
+++ myworld/my_tennis_club/members/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='home'),
     path('members/', views.members, name='members'),
+    path('members/member_details/<int:id>', views.member_details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
